909e5b49dc48d4384ed20ad8aece71e39e8e201b
yunisaliosman <yunis.osman@student.moringaschool.com>
2023-02-13 11:28:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ function saturdayFun(activity){
         else{return `This Saturday, I want to `+ activity +`!` }
     }
     saturdayFun();
-    
+  
     const mondayWork = function(activity){
         if(activity == null){return `This Monday, I will go to the office.`}
         else{return `This Monday, I will `+ activity +`.` }
     }
     mondayWork();
-    
-    
-    function wrapAdjective(name){
-        return function wrap(emphatic){
-            return `You are ${name}`+ emphatic + `${name}!`;
+
+    function wrapAdjective(yonkaal){
+        return  function wrap(sumaya){
+            return `You are ${yonkaal}`+ sumaya + `${yonkaal}!`;
+
         }
-    return wrap();
-        }
-        
-    wrapAdjective('*')('a hard worker');
+        wrapAdjective("*")("special");
+    }
